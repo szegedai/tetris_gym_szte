3f53990158ca57736994cf0c26e01ab5b8cf08b9
Zsolt Szanto <zsozso121@gmail.com>
2023-04-08 11:49:25 +0200

    add agent evaluation

--- tetris_gym/utils/eval_utils.py
+++ tetris_gym/utils/eval_utils.py
@@ -1,8 +1,7 @@
 from gym.wrappers import Monitor
 
-def evaluate(env, model, ep_num=100, seed=0):
+def evaluate(env, model, ep_num=100):
     env_test = env
-    env_test.seed(seed)
 
     sum_reward = 0
 
@@ -25,7 +24,31 @@ def evaluate(env, model, ep_num=100, seed=0):
 
     return sum_reward / ep_num
 
-def create_videos(env, model, ep_num=2, seed=0, folder="videos"):
+def evaluate_agent(env, agent, ep_num=100):
+    env_test = env
+
+    sum_reward = 0
+
+    for _ in range(ep_num): 
+      
+        obs = env_test.reset()
+        score = 0
+        done = False
+
+        while not done:
+            action, _ = agent.act(obs)
+
+            obs, reward, done, _ = env_test.step(action)
+
+            score += reward
+
+        sum_reward += score
+        
+    env_test.close()
+
+    return sum_reward / ep_num
+
+def create_videos(env, model, ep_num=2, folder="videos"):
     env = Monitor(env, folder, force=True, video_callable=lambda episode_id: True)
 
     sum_reward = 0
