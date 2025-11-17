71de3c37c113488133ac3117881beba56c532d5d
Zsolt Szántó <zsozso121@gmail.com>
2025-11-17 20:56:16 +0100

    fix: update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -3,7 +3,7 @@ from tetris_gym.envs.tetris_gym import TetrisGym
 from tetris_gym.utils.eval_utils import evaluate_agent
 
 # Környezet létrehozása
-env = TetrisGym(width=10, height=20)
+env = TetrisGym(width=8, height=16)
 
 agent = Agent(env)
 
